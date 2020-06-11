23ce6ff624de480c1c30cf167fbfba91f3780fce
L3n1ad <danieltoth0910@gmail.com>
2020-06-11 11:02:25 +0100

    create songList component

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,14 @@
 import React from "react";
 // import a named export
 // import { selectSong } from '../actions';
+import SongList from "./SongList";
 
 const App = () => {
-  return <div>HELLO</div>;
+  return (
+    <div>
+      <SongList />
+    </div>
+  );
 };
 
 export default App;
