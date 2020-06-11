9fdfd32a4a878813f4be74f626324c9a8dd74b9e
L3n1ad <danieltoth0910@gmail.com>
2020-06-11 13:07:06 +0100

    get states as props from redux and display it as a list

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const SongList = () => {
-  return <div>I am songList component</div>;
+const SongList = (props) => {
+  function renderList() {
+    return props.songs.map((song, index) => {
+      return (
+        <div className="item" key={index}>
+          <div className="right floated content">
+            <button className="ui button primary">Select</button>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+      );
+    });
+  }
+
+  return <div className="ui divided list">{renderList()}</div>;
 };
 
-export default SongList;
+// it's the convention to call it this way
+// the function will get our redux states to turn it into props we can use in our component
+const mapStateToProps = (state) => {
+  return { songs: state.songs };
+};
+
+export default connect(mapStateToProps)(SongList);
